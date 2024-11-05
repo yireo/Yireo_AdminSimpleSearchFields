112310cdd3dc9e42024aa36d8ac7ca777f41b4cf
Jisse Reitsma <jisse@yireo.com>
2024-11-05 16:29:58 +0100

    Add abstract method

--- DataProvider/AbstractItemProvider.php
+++ DataProvider/AbstractItemProvider.php
@@ -18,26 +18,28 @@ abstract class AbstractItemProvider implements ItemProviderInterface
         protected RequestInterface $request
     ) {
     }
-    
+
     abstract public function getItems(): array;
-    
+
+    abstract public function getSearchableFields(): array;
+
     public function getSearchCriteria(): SearchCriteria
     {
         $this->searchCriteriaBuilder->setPageSize($this->getLimit());
         $this->searchCriteriaBuilder->setCurrentPage($this->getPage());
-        
+
         if ($this->hasSearch()) {
             $this->searchCriteriaBuilder->addFilters($this->getSearchFilters());
         }
-        
+
         return $this->searchCriteriaBuilder->create();
     }
-    
+
     public function getSearch(): string
     {
         return (string)$this->request->getParam('search');
     }
-    
+
     public function getSearchFilters(): array
     {
         $filters = [];
@@ -48,33 +50,33 @@ abstract class AbstractItemProvider implements ItemProviderInterface
                 ->setValue('%'.$this->getSearch().'%')
                 ->create();
             }
-        
+
         return $filters;
     }
-    
+
     public function hasSearch(): bool
     {
         $search = $this->getSearch();
         return !empty($search);
     }
-    
+
     public function getLimit(): int
     {
         $limit = (int)$this->request->getParam('limit');
         if ($limit) {
             return $limit;
         }
-        
+
         return 10;
     }
-    
+
     public function getPage(): int
     {
         $page = (int)$this->request->getParam('page');
         if ($page) {
             return $page;
         }
-        
+
         return 0;
     }
 }
